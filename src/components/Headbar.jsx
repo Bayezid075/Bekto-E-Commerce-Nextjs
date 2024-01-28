9d4e1517f4a620a8f6072a9d64a84b9adc67d51b
Bayezid075 <mrbayezidhoshen@gmail.com>
2024-01-28 14:46:27 +0600

    some work of headbar

--- src/components/Headbar.jsx
+++ src/components/Headbar.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 import styles from "./styles/Headbar.module.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faEnvelope,  } from "@fortawesome/free-regular-svg-icons";
+import { faPhone  } from "@fortawesome/free-solid-svg-icons";
+
+
+
 export default function Headbar() {
-  return <div></div>;
+
+
+  return <div className={styles.mainBar}>
+    <div className="styles.left_side">
+      <ul className={styles.leftList}>
+        <li>  
+            <span><FontAwesomeIcon className={styles.icons} icon={faEnvelope} /></span>
+            <span className={styles}>mrbayezidhoshen@gmail.com</span>
+            
+        </li>
+        <li>
+              <span><FontAwesomeIcon icon={faPhone} className={styles.icons}/></span>
+              <span>+880-1728181464</span>
+          </li>
+      </ul>
+    </div>
+    <div className="style.right_side">
+
+    </div>
+  </div>;
 }
